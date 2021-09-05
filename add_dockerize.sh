fab5ceb889ddba733978e6804437b774ac982fc5
David Howell <david@davidjameshowell.com>
2021-09-05 04:57:20 +0000

    Update to use uname a instead of r

--- add_dockerize.sh
+++ add_dockerize.sh
@@ -1,4 +1,4 @@
-ARCH=$(uname -r)
+ARCH=$(uname -a)
 SUB="aarch64"
 
 DOCKERIZE_ARCH="amd64"
@@ -8,6 +8,11 @@ if grep -q "$SUB" <<< "$ARCH"; then
   echo "ARM detected, set arch version"
   DOCKERIZE_ARCH="armhf"
 fi
+
+apt-get update \
+&& apt-get install -y --no-install-recommends wget ca-certificates openssl \
+&& apt-get purge -y --auto-remove wget ca-certificates openssl \
+&& rm -rf /var/lib/apt/lists/*
 
 wget https://github.com/jwilder/dockerize/releases/download/${DOCKERIZE_VERSION}/dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz
 tar -C /usr/local/bin -xzvf dockerize-linux-${DOCKERIZE_ARCH}-${DOCKERIZE_VERSION}.tar.gz
